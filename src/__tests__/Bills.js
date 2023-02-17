0a4dbade73f18fd717a52991551c544f40854d0d
Nadia Bolivant <chelseasama@gmail.com>
2023-02-17 17:25:58 +0100

    Integration tests added to bill test file.

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -10,7 +10,10 @@ import { ROUTES, ROUTES_PATH } from '../constants/routes.js';
 import { localStorageMock } from '../__mocks__/localStorage.js';
 import Bills from '../containers/Bills.js';
 import store from '../__mocks__/store.js';
+import mockStore from '../__mocks__/store';
 import router from '../app/Router.js';
+
+jest.mock('../app/store', () => mockStore);
 
 describe('Given I am connected as an employee', () => {
     describe('When I am on Bills Page', () => {
@@ -187,5 +190,65 @@ describe('When I am on Bills Page and I click on the icon Eye', () => {
 
         //On vérifie alors si on a bien le message "justificatif" qui est présent en haut de la modale
         expect(screen.getAllByText('Justificatif')).toBeTruthy();
+    });
+});
+
+// ----- test d'intégration GET -----
+describe('Given I am a user connected as Employee', () => {
+    describe('When I navigate to Bills page', () => {
+        test('fetches bills from mock API GET', async () => {
+            localStorage.setItem('user', JSON.stringify({ type: 'Employee', email: 'a@a' }));
+            const root = document.createElement('div');
+            root.setAttribute('id', 'root');
+            document.body.append(root);
+            router();
+            window.onNavigate(ROUTES_PATH.Bills);
+            await waitFor(() => expect(screen.getByText('Mes notes de frais')).toBeTruthy());
+        });
+        describe('When an error occurs on API', () => {
+            beforeEach(() => {
+                jest.spyOn(store, 'bills');
+                Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+                window.localStorage.setItem(
+                    'user',
+                    JSON.stringify({
+                        type: 'Employee',
+                        email: 'a@a',
+                    }),
+                );
+                const root = document.createElement('div');
+                root.setAttribute('id', 'root');
+                document.body.appendChild(root);
+                router();
+            });
+            test('fetches bills from an API and fails with 404 message error', async () => {
+                store.bills.mockImplementationOnce(() => {
+                    return {
+                        list: () => {
+                            return Promise.reject(new Error('Erreur 404'));
+                        },
+                    };
+                });
+                window.onNavigate(ROUTES_PATH.Bills);
+                await new Promise(process.nextTick);
+                const message = await screen.getByText(/Erreur 404/);
+                expect(message).toBeTruthy();
+            });
+
+            test('fetches messages from an API and fails with 500 message error', async () => {
+                store.bills.mockImplementationOnce(() => {
+                    return {
+                        list: () => {
+                            return Promise.reject(new Error('Erreur 500'));
+                        },
+                    };
+                });
+
+                window.onNavigate(ROUTES_PATH.Bills);
+                await new Promise(process.nextTick);
+                const message = await screen?.getByText(/Erreur 500/);
+                expect(message).toBeTruthy();
+            });
+        });
     });
 });
